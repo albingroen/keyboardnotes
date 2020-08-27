643c236fe3681104237d1bcc46ccb1326fd64a0d
Albin Groen <albin.groen@gmail.com>
2020-08-27 07:58:46 +0200

    :wrench: Set up bugsnag

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,38 @@ import App from "./App";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth0-provider-with-history";
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
 
 import * as serviceWorker from "./serviceWorker";
 import store from "./store";
 
+Bugsnag.start({
+  apiKey: 'f1465297eadd679d0849362d4f2acb76',
+  plugins: [new BugsnagPluginReact()]
+})
+
+const ErrorBoundary = Bugsnag.getPlugin('react')?.createErrorBoundary(React)
+
+const Application = () => (
+  <Router>
+    <Auth0ProviderWithHistory>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0ProviderWithHistory>
+  </Router>
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Auth0ProviderWithHistory>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </Auth0ProviderWithHistory>
-    </Router>
+    {ErrorBoundary ? (
+      <ErrorBoundary>
+        <Application />
+      </ErrorBoundary>
+    ) : (
+      <Application />
+    )}
   </React.StrictMode>,
   document.getElementById("root")
 );
