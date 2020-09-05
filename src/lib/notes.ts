eb0944c2b02d1a0f130b2d186d6f770806953fd2
Albin Groen <albin.groen@gmail.com>
2020-09-05 15:07:23 +0200

    :recycle: Patch default example note

--- src/lib/notes.ts
+++ src/lib/notes.ts
@@ -1,7 +1,6 @@
 export const WELCOME_NOTE = {
   _id: "101",
   title: "Welcome to Keyboardnotes 👋",
-  body:
-    '# Heading 1\n## Heading 2\n### Heading 3\n#### Heading 4\n##### Heading 5\n###### Heading 6\n\n__strong__\n_italic_\n`code`\n~~Striked.~~\n[link](https://example.com)\n\n## Unordered list\n\n- List item\n  - List item\n    - List item\n\n## Ordered list\n\n1. List item\n2. List item\n3. List item\n\n## Todo list\n\n- [ ] Todo item 1\n  - [ ] Todo item 2\n    - [x] Todo item 3\n\n## Table\n\n| Tables   |      Are      |  Cool |\n|----------|:-------------:|------:|\n| col 1 is |  left-aligned | $1600 |\n\n## Divider\n\n---\n\n## Image\n\n![placeholder](https://placehold.it/125)\n\n## Blockquote\n\n> This is a cool block quote\n\n## Code block\n\n```\n{\n  "firstName": "John",\n  "lastName": "Smith",\n  "age": 25\n}\n```',
+  body: '> Hit enter to go into edit mode\n\n## This is the preview\n\nWhat you see here is the preview of the note your currently "standing on". \n\n## Format\nNotes are formatted using [markdown](https://en.wikipedia.org/wiki/Markdown) and contain a superset of features that allows you to create things like tables, dropdowns and checkboxes. \n\n## Why?\nI built this tool for myself, since I couldn\'t find any good note taking app that store notes online and was completely accessible through the keyboard. I also didn\'t want to bother grasping obscure special concepts specific to each app. I just wanted a simple app where I could easily jot down things that come to mind throughout the day, that\'s quickly accessible through any browser and where I wouldn\'t have to touch the mouse or trackpad.',
   createdAt: new Date().toString(),
 };
